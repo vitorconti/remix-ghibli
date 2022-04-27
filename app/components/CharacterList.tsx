b0c48855d0ab5a91bbfa6b9df98c43bdad38c70e
vitorcontiz <vitorcontizgit@gmail.com>
2022-04-27 00:37:35 -0300

    refactor: add prop key in li tag

--- app/components/CharacterList.tsx
+++ app/components/CharacterList.tsx
@@ -1,26 +1,25 @@
-import { NavLink } from "remix";
-import { FilmCharacter } from "~/api/film";
+import { NavLink } from 'remix'
+import { FilmCharacter } from '~/api/film'
 
 type CharacterListProps = {
-  characters?: FilmCharacter[];
-};
+  characters?: FilmCharacter[]
+}
 
 export default function CharacterList({ characters }: CharacterListProps) {
   return (
-    <div className="flex-1 max-w-md">
-      <h3 className="text-3xl">Characters</h3>
-      <ul className="flex flex-col space-y-3 my-3">
+    <div className='flex-1 max-w-md'>
+      <h3 className='text-3xl'>Characters</h3>
+      <ul className='flex flex-col space-y-3 my-3'>
         {characters?.map((character) => (
-          <li>
+          <li key={character.id}>
             <NavLink
-              prefetch="intent"
-              key={character.id}
+              prefetch='intent'
               to={`characters/${character.id}`}
               className={({ isActive }) =>
                 `w-full hover:underline p-3 rounded border border-slate-400 inline-block font-sans font-semibold text-lg ${
                   isActive
-                    ? "bg-gray-600 text-white font-bold border-2"
-                    : "text-white "
+                    ? 'bg-gray-600 text-white font-bold border-2'
+                    : 'text-white '
                 } `
               }
             >
@@ -30,5 +29,5 @@ export default function CharacterList({ characters }: CharacterListProps) {
         ))}
       </ul>
     </div>
-  );
+  )
 }
